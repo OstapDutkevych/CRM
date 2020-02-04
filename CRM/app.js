ecc46c71a6fbc15694515563c715800e2eafaf89
OstapDutkevych <vishnyaostap88@gmail.com>
2020-02-04 13:40:53 +0200

    added models

--- CRM/app.js
+++ CRM/app.js
@@ -10,9 +10,9 @@ const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
 app.use(require('morgan')('dev'));
-app.use(require('cors')());
 // app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(require('cors')());
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
