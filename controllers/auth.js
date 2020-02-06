3585959d03015a79d12ac638a0cd308fae73ef59
OstapDutkevych <vishnyaostap88@gmail.com>
2020-02-06 14:53:12 +0200

    added loginUser func and hash password on register, added token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,40 @@
-const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-module.exports.loginUser = (req, res) => {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+const User = require("../models/User");
+const keys = require("../config/keys");
+
+module.exports.loginUser = async (req, res) => {
+  const candidate = await User.findOne({ email: req.body.email });
+
+  if (candidate) {
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      candidate.password
+    );
+    if (checkPassword) {
+      const token = jwt.sign(
+        {
+          email: candidate.email,
+          userId: candidate._id
+        },
+        keys.jwt,
+        { expiresIn: 60 * 60 }
+      );
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      });
+    } else {
+      res.status(401).json({
+        message: "wrong password, try again"
+      });
     }
-  });
+  } else {
+    res.status(404).json({
+      message: "User is not registered 404(Not Found)"
+    });
+  }
 };
 
 module.exports.registerUser = async (req, res) => {
@@ -16,9 +44,11 @@ module.exports.registerUser = async (req, res) => {
       message: "Email already exists, try another one"
     });
   } else {
+    const salt = bcrypt.genSaltSync(10);
+    const password = req.body.password;
     const user = new User({
       email: req.body.email,
-      password: req.body.password
+      password: bcrypt.hashSync(password, salt)
     });
     try {
       await user.save();
