afc1825b7b1b17ddab1f743b0d7b634e40d67754
OstapDutkevych <vishnyaostap88@gmail.com>
2020-02-07 13:43:54 +0200

    added functionality order

--- controllers/order.js
+++ controllers/order.js
@@ -1,3 +1,52 @@
-module.exports.getOrder = (req, res) => {};
+const OrderModel = require("../models/Order");
+const errorHandler = require("../utils/errorHandler");
+//localhost:5000/api/order?offset=2&limit=5
+module.exports.getOrder = async (req, res) => {
+  const query = {
+    user: req.user.id
+  };
+  if (req.query.start) {
+    query.date = {
+      //більше або рівне
+      $gte: req.query.start
+    };
+  }
+  if (req.query.end) {
+    if (!query.date) {
+      query, (date = {});
+    }
+    query.date["$lte"] = req.query.end;
+  }
 
-module.exports.createOrder = (req, res) => {};
+  if (req.query.order) {
+    query.order = +req.query.order;
+  }
+  try {
+    const orders = await OrderModel.find(query)
+      .sort({ date: -1 })
+      .skip(+req.query.offset)
+      .limit(+req.query.limit);
+    res.status(200).json(order);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
+
+module.exports.createOrder = async (req, res) => {
+  try {
+    const lastOrder = await OrderModel.findOne({ user: req.user.id }).sort({
+      date: -1
+    });
+    const maxOrder = lastOrder ? lastOrder.order : 0;
+    const order = new OrderModel({
+      list: req.body.list,
+      user: req.user.id,
+      order: maxOrder + 1
+    });
+
+    await order.save();
+    res.status(201).json(order);
+  } catch (e) {
+    errorHandler(res, e);
+  }
+};
--- routes/order.js
+++ routes/order.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+
 const controller = require("../controllers/order");
 
-router.get("/", controller.getOrder);
-router.post("/", controller.createOrder);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.getOrder
+);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  controller.createOrder
+);
 
 module.exports = router;
