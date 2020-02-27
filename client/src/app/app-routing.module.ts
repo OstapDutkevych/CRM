0efcb8e371fba7bcdc3235cd01168fffe4f0929c
Ostap Dutkevych <ostap.dutkevych@etiya.com>
2020-02-27 17:46:22 +0200

    implements router navigate for auth

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,30 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { LoginPageComponent } from "./login-page/login-page.component";
+import { SiteLayoutComponent, AuthLayoutComponent } from "./shared/layouts";
+import { RegisterPageComponent } from "./register-page/register-page.component";
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
-    path: "login",
-    component: LoginPageComponent
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginPageComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'register',
+        component: RegisterPageComponent
+      }
+    ]
+  },
+  {
+    path: 'home',
+    component: SiteLayoutComponent,
+    children: []
   }
 ];
 
---
+++ client/src/app/shared/layouts/index.ts
@@ -0,0 +1,2 @@
+export * from "./auth-layout/auth-layout.component";
+export * from "./site-layout/site-layout.component";
